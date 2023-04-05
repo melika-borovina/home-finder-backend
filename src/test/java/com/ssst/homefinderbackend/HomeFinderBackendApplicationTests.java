c8d690253e3bc19e6ca61b044c452c9e631a2d99
Melika Borovina <127191580+melika-borovina@users.noreply.github.com>
2023-04-05 15:11:09 +0200

    initial commit
    I created a starter Spring Boot project using Maven, called home-finder-backend. In the pom.xml file, I added several dependencies required for building a RESTful web service using Spring Boot

---
+++ src/test/java/com/ssst/homefinderbackend/HomeFinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssst.homefinderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
