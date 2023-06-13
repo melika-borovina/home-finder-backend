93b660dc296566b2e7a9ef6ead67c206cb9592ae
zakir <zakira.skaljic@gmail.com>
2023-06-13 04:55:43 +0200

    updated review test

--- src/test/java/com/ssst/homefinderbackend/data/ReviewTest.java
+++ src/test/java/com/ssst/homefinderbackend/data/ReviewTest.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class ReviewTest {
 
     private static Integer id1 = 1;
+    private static Double rating1 = 2.0;
     private static String title1 = "Ewww";
     private static String description1 = "Hated it";
     private static RealEstateEntity realestate1;
@@ -22,6 +23,7 @@ public class ReviewTest {
     public static ReviewEntity review() {
         ReviewEntity review = new ReviewEntity();
         review.setId(id1);
+        review.setRating(rating1);
         review.setTitle(title1);
         review.setDescription(description1);
         review.setRealEstateId(realestate1);
@@ -30,10 +32,10 @@ public class ReviewTest {
     }
 
     public static ReviewDto reviewDto1() {
-        return new ReviewDto(id1, title1, description1, realestate1);
+        return new ReviewDto(id1, rating1, title1, description1, realestate1);
     }
 
     public static ReviewDto reviewDto2() {
-        return new ReviewDto(3, "Loved it", "Loveeee", realestate1);
+        return new ReviewDto(3, 2.1,"Loved it", "Loveeee", realestate1);
     }
 }
